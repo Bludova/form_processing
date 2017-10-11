b36f83cea11f1f1721f2fdd7514b13fade7f0c57
Bludova <32148601+Bludova@users.noreply.github.com>
2017-10-12 01:56:06 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+  <html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Список загруженных тестов</title>
+    <style>
+   .error {
+    color: red;
+    font-size: 120%;
+   } 
+   .correctly {
+    color: #00A72A; 
+    font-size: 120%; 
+   } 
+  </style>
+  </head>
+  <body>
+    <h1>Пройдите тест</h1>
+ <?php
+$filelist = glob("*.json");
+$json = file_get_contents(__DIR__ ."./$filelist[0]");
+$data = json_decode($json, true);
+$issues_1 = $data["question_1"];
+$issues_2 = $data["question_2"];
+
+
+?>
+      <form action=" " method="get">
+        <fieldset name="q1">
+          <legend><?= $issues_1["label"]; ?></legend>
+          <label><input type="radio" name="q1" value='<?= $issues_1["option_1"];?>' ><?= $issues_1["option_1"]; ?></label>
+          <label><input type="radio" name="q1" value='<?= $issues_1["option_2"];?>' ><?= $issues_1["option_2"]; ?></label>
+          <label><input type="radio" name="q1" value='<?= $issues_1["option_3"];?>' ><?= $issues_1["option_3"]; ?></label>
+          <label><input type="radio" name="q1" value='<?= $issues_1["option_4"];?>' ><?= $issues_1["option_4"]; ?></label>
+        </fieldset>
+        <fieldset name="q2">
+          <legend><?= $issues_2["label"]; ?></legend>
+          <label><input type="radio" name="q2" value='<?= $issues_2["option_1"];?>' ><?= $issues_2["option_1"]; ?></label>
+          <label><input type="radio" name="q2" value='<?= $issues_2["option_2"];?>' ><?= $issues_2["option_2"]; ?></label>
+          <label><input type="radio" name="q2" value='<?= $issues_2["option_3"];?>' ><?= $issues_2["option_3"]; ?></label>
+          <label><input type="radio" name="q2" value='<?= $issues_2["option_4"];?>' ><?= $issues_2["option_4"]; ?></label>
+        </fieldset>
+        <input type="submit" value="Отправить">
+      </form>
+<?php
+
+if(isset($_GET['q1']) && !empty($_GET['q1']))
+{
+ if($_GET['q1'] ==  $issues_1["result"]) 
+  {
+    echo "На вопрос: " .$issues_1["label"]."  ";
+ ?> <span class="correctly"> Вы ответили верно! </span><?php echo "<br>";
+  }else 
+  {
+   echo "На вопрос: " .$issues_1["label"]."  ";
+    ?> <span class="error">Вы ответили неправельно!</span> Правельный ответ:"
+   <?php echo $issues_1["result"]. " <br>" ;
+  }
+}
+
+if(isset($_GET['q2']) && !empty($_GET['q2']))
+{
+ if($_GET['q2'] ==  $issues_2["result"]) 
+  {
+    echo "На вопрос: " .$issues_2["label"]."  ";
+   ?><span class="correctly">Вы ответили верно!</span>.<?php echo "<br>";
+  }else 
+  {
+   echo "На вопрос: " .$issues_2["label"]."  ";
+   ?>"<span class="error">Вы ответили неправельно!</span> Правельный ответ:" <?php echo$issues_2["result"]. "<br>" ;
+  }
+}
+?>
+<hr>
+ <a href="./list.php">Выбрать тест! </a>
+ <a href="./admin.php">Загрузить тест! </a>
+  </body>
+</html>
