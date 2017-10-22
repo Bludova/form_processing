3af28febedc1ddb06a190da0e7cfc62460457f64
Bludova <32148601+Bludova@users.noreply.github.com>
2017-10-22 23:31:23 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -8,11 +8,26 @@
 <h1>Загрузить JSON-файл c тестом.</h1>
 
 
-<form enctype="multipart/form-data" action="./list.php" method="POST">
+<form enctype="multipart/form-data" action="" method="POST">
 Загрузить JSON-файл <input name="userfile" type="file">
 <br>
 <input type="submit" value="Отправить">
 </form>
+<?php
+if(isset($_FILES['userfile'] ['name']) && !empty($_FILES['userfile'] ['name']))
+{
+  if($_FILES['userfile'] ['error'] == UPLOAD_ERR_OK &&
+    move_uploaded_file($_FILES['userfile'] ['tmp_name'], $_FILES['userfile'] ['name']))
+  {
+    echo "Файл с текстом загружен! <br>";
+  }else 
+  {
+    echo " Ошибка: Файл с текстом не загружен! <br>";
+
+  }
+}
+
+?>
 <hr>
 <a href="./list.php">Выбрать тест! </a>
   </body>
